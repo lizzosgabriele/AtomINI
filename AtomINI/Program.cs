b644963d513a825ea04b6caf06d9434a144ed582
Lizzos Gabriele <gabriele.lizzos@atom.it>
2025-09-03 15:14:57 +0200

    started project

--- AtomINI/Program.cs
+++ AtomINI/Program.cs
@@ -1,6 +1,29 @@
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
+
 namespace AtomINI {
     internal class Program {
+        
+        static LoggingLevelSwitch loggingLevelSwitch = new LoggingLevelSwitch(LogEventLevel.Verbose);
+
+        
         public static void Main(string[] args) {
+            
+            Logger log = new LoggerConfiguration()
+                .MinimumLevel.ControlledBy(loggingLevelSwitch)
+                //.MinimumLevel.Verbose()
+                .Enrich.WithProperty("SourceContext", "ILFashion")
+                .WriteTo.Console(
+                    outputTemplate: "{Timestamp:HH:mm:ss.fff} [{SourceContext}] [{Level:u}] - {Message}{NewLine}{Exception}")
+                .WriteTo.File(
+                    path: "ILFashion-.log", 
+                    rollingInterval: RollingInterval.Day,
+                    outputTemplate: "{Timestamp:HH:mm:ss.fff} [{SourceContext}] [{Level:u}] - {Message}{NewLine}{Exception}"
+                )
+                .CreateLogger();
+            
+            Log.Logger = log;
         }
     }
 }
